cc2c20fab0b7bd1a5ad16879490e581f31bf9ef5
Tyler Swindell <electromagnetic.jolt@gmail.com>
2018-12-31 06:47:12 +0000

    added quantity cap and email intent logic

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,11 +9,16 @@
 package com.example.android.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -22,7 +27,7 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    int quantity = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,37 +38,68 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
+
         CheckBox whipBox = (CheckBox) findViewById(R.id.iswhipped);
+        CheckBox chocolateBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        EditText name_box = findViewById(R.id.name_box);
+        String orderName = name_box.getText().toString();
         boolean isWhipped = whipBox.isChecked();
-        displayMessage(createOrderSummary(price, isWhipped));
+        boolean withChocolate = chocolateBox.isChecked();
+        int price = calculatePrice(isWhipped,withChocolate);
+        String orderSummary = (createOrderSummary(price, isWhipped, withChocolate, orderName));
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + orderName);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 
     /**
      * Calculates the price of the order.
      *
-     * @param quantity is the number of cups of coffee ordered
+     * quantity is the number of cups of coffee ordered
      */
-    private int calculatePrice() {
+    private int calculatePrice(boolean withWhip, boolean withChocolate) {
         int price = quantity * 5;
+        if (withWhip)
+        {price=price + quantity;}
+        if (withChocolate)
+        {price= price + (quantity * 2); }
+
         return price;
     }
 
-    private String createOrderSummary(int price, boolean isWhipped)
+    private String createOrderSummary(int price, boolean isWhipped, boolean withChocolate, String orderName)
 
     {
-        return "Name: Tyler Swindell\n" +
-                "Add whipped cream? " + isWhipped + "\nQuantity: " + quantity + "\n" +
+        return "Name: " + orderName + "\n" +
+                "Add whipped cream? " + isWhipped +
+                "\nAdd chocolate? " + withChocolate +
+                "\nQuantity: " + quantity + "\n" +
                 "Total: $" + price + "\n" +
                 "Thank you!";
     }
 
     public void increment(View view) {
-        quantity = quantity+1;
+
+
+        if (quantity == 100)
+        { Toast.makeText(this, "You cannot order more than 100 cups of coffee per order.", Toast.LENGTH_SHORT).show();
+            return;}
+            else
+            quantity = quantity+1;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity == 1)
+        {
+            Toast.makeText(this, "You must order at least one cup of coffee per order.", Toast.LENGTH_SHORT).show();
+            return;}
+        else
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
@@ -74,15 +110,6 @@ public class MainActivity extends AppCompatActivity {
     private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + numberOfCoffees);
-    }
-
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
     }
 
 }
